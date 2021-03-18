23a3522be8fb2261f70971f8b3b73ec3c8e12e72
Ian Bryant <ijb2020syr@gmail.com>
2021-03-17 21:09:48 -0400

    flashlight effect

--- src/shaders/diffuse_frag.glsl
+++ src/shaders/diffuse_frag.glsl
@@ -17,8 +17,9 @@ uniform sampler2D myTextureSampler;
 void main(){
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
+    float invlen1 = 2.0f/length(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = diff * lightColor * invlen1;
     vec3 ambient = 0.2f * lightColor;
     color = (ambient + diffuse) * texture(myTextureSampler, UV).rgb;
 
